ca4009783dcc954b0ba4330aad80641687bb47a3
reddynarasimha393-an <reddynarasimha393@email.com>
2026-01-06 20:28:48 +0530

    Initial commit with Python programs

---
+++ class.python.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    
+    def display(self):
+        print(f"Car: {self.brand} {self.model}")
+
+car1 = Car("Toyota", "Corolla")
+car1.display()
